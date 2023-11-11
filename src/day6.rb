efdbf64c3a5bc609ebb48b81c7d3b8071efcc5c7
Lukas Schmid <lukasschmid97@gmail.com>
2023-11-11 09:27:14 +0100

    day6

---
+++ src/day6.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+input_string = nil
+File.open 'input/day6_input.txt' do |puzzle_in|
+  input_string = puzzle_in.read
+end
+
+# input_string = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+def first_unique_sequence_of(n, string)
+  stack = string.chars[...n]
+  string.chars[n..].each_with_index do |char, index|
+    if stack.uniq == stack
+      puts index + n
+      break
+    end
+    stack.push(char)
+    stack.shift
+  end
+end
+
+first_unique_sequence_of(4, input_string)
+first_unique_sequence_of(14, input_string)
